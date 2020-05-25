9773fc831a85d1b520bfa16c4e3f904e4740d143
Kaushik ASP <aspkaushik@gmail.com>
2020-05-26 04:58:34 +0530

    feat: Adds a GET request to fetch all sites associated with a hardcoded user id

--- frontend/src/app/sites/sites.component.ts
+++ frontend/src/app/sites/sites.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { SitesService } from './sites.service';
 
 @Component({
   selector: 'app-sites',
   templateUrl: './sites.component.html',
-  styleUrls: ['./sites.component.sass']
+  styleUrls: ['./sites.component.sass'],
+  providers: [SitesService],
 })
 export class SitesComponent implements OnInit {
-
-  constructor() { }
+  private sites;
+  constructor(private sitesService: SitesService) {}
 
   ngOnInit(): void {
+    this.sitesService.getSites().subscribe((data: any[]) => {
+      console.log(data);
+    });
   }
-
 }
