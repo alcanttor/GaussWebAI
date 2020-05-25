6757404009ea9e4fe1049cec626e5442277c3ba6
Kaushik ASP <aspkaushik@gmail.com>
2020-05-26 04:32:50 +0530

    fix: Adds more bootstrap related files for angular integration

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,23 +7,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
-import { MaterialModule } from './material-module';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { AuthService } from './auth.service';
 import { SitesComponent } from './sites/sites.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, DashboardComponent, SitesComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    SitesComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule,
-    FlexLayoutModule,
     FormsModule,
     ReactiveFormsModule,
+    NgbModule,
   ],
   providers: [AuthService],
   bootstrap: [AppComponent],
