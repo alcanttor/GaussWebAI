49b24d66bb3bd3798ae64a5f2ba76428ff3dacba
Kaushik ASP <aspkaushik@gmail.com>
2020-05-26 04:33:53 +0530

    fix: Comments code temporarily. Work to be resumed on this once dashboard is done

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -6,22 +6,6 @@ import {
   Validators,
 } from '@angular/forms';
 import { AuthService } from '../auth.service';
-import { ErrorStateMatcher } from '@angular/material/core';
-
-/** Error when invalid control is dirty, touched, or submitted. */
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(
-    control: FormControl | null,
-    form: FormGroupDirective | NgForm | null
-  ): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(
-      control &&
-      control.invalid &&
-      (control.dirty || control.touched || isSubmitted)
-    );
-  }
-}
 
 @Component({
   selector: 'app-login',
@@ -29,15 +13,6 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   styleUrls: ['./login.component.sass'],
 })
 export class LoginComponent implements OnInit {
-  emailFormControl = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
-  emailMatcher = new MyErrorStateMatcher();
-
-  passwordFormControl = new FormControl('', [Validators.required]);
-  passwordMatcher = new MyErrorStateMatcher();
-
   constructor(private auth: AuthService) {}
 
   ngOnInit(): void {}
