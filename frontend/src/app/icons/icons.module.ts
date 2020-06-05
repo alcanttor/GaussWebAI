b182ee28607b82743490c7eaacfbdc0fc9fbf030
Kaushik ASP <aspkaushik@gmail.com>
2020-06-06 03:32:08 +0530

    feat: Imports more icons

--- frontend/src/app/icons/icons.module.ts
+++ frontend/src/app/icons/icons.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 
 import { FeatherModule } from 'angular-feather';
-import { Plus, Edit2, Trash2 } from 'angular-feather/icons';
+import { Plus, Edit2, Trash2, MoreVertical, Menu } from 'angular-feather/icons';
 
 // Select some icons (use an object, not an array)
 const icons = {
   Plus,
   Edit2,
   Trash2,
+  MoreVertical,
+  Menu,
 };
 
 @NgModule({
