8a84b1a08470f6493ee8c8009f69a94acd8b580b
Kaushik ASP <aspkaushik@gmail.com>
2020-06-06 00:18:21 +0530

    feat: Adds proper type definition for Site model

--- frontend/src/app/shared/models/site.ts
+++ frontend/src/app/shared/models/site.ts
@@ -1,7 +1,15 @@
 export interface Site {
-  name: string;
   id: string;
+  name: string;
   connector: {
     id: string;
+    name?: string;
+  };
+  createdDate?: string;
+  updatedDate?: string;
+  siteToken: {
+    id?: number;
+    token: string;
+    isValid?: boolean;
   };
 }
