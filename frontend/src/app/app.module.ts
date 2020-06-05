325845c8282d3b600bb3a9fdff3fa066aef21fa9
Kaushik ASP <aspkaushik@gmail.com>
2020-06-06 00:19:58 +0530

    feat: Imports all new modules and components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,12 +7,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IconsModule } from './icons/icons.module';
+
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { AuthService } from './auth.service';
 import { SitesComponent } from './sites/sites.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ListActionsComponent } from './shared/components/list-actions/list-actions.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +23,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     LoginComponent,
     DashboardComponent,
     SitesComponent,
+    ListActionsComponent,
   ],
   imports: [
     BrowserModule,
@@ -29,6 +33,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     ReactiveFormsModule,
     NgbModule,
     HttpClientModule,
+    IconsModule,
   ],
   providers: [AuthService],
   bootstrap: [AppComponent],
