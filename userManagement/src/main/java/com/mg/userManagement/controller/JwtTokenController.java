18fce848b457129494a75e554fc26286045ec0fc
CharanS05 <charan.singh@alcanttor.com>
2020-06-08 23:33:33 +1000

    Rollback fix changes

--- userManagement/src/main/java/com/mg/userManagement/controller/JwtTokenController.java
+++ userManagement/src/main/java/com/mg/userManagement/controller/JwtTokenController.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mg.userManagement.service.JwtService;
