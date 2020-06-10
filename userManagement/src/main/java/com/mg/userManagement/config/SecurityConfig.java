150665ee5a620d9dbc45370784e34fcbdbaf50f6
Varun <luna.varun@gmail.com>
2020-06-10 07:38:12 +0530

    option method allowed

--- userManagement/src/main/java/com/mg/userManagement/config/SecurityConfig.java
+++ userManagement/src/main/java/com/mg/userManagement/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.mg.userManagement.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -46,12 +47,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.csrf().disable().authorizeRequests().antMatchers("/token/**").permitAll()
+		httpSecurity.csrf().disable().cors().and().
+		authorizeRequests().antMatchers("/token/**").permitAll()
 			.antMatchers("/swagger-ui.html/**").permitAll()
 			.antMatchers("/saveUser/**").permitAll()
+			.antMatchers(HttpMethod.OPTIONS).permitAll()
 			.anyRequest().authenticated().and().exceptionHandling().and().sessionManagement()
 			.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		httpSecurity.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+		httpSecurity.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class).httpBasic();
 	}
 	
 	@Override
